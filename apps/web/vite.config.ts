40b5e595c6d757d40fdcbe1113b526f808ae793b
tampham512 <phamthanhtam512@gmail.com>
2023-09-12 23:14:33 +0700

    uppp

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import macrosPlugin from 'vite-plugin-babel-macros';
 import svgr from 'vite-plugin-svgr';
-
 import * as path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
