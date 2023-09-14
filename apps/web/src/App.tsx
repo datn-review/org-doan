07795ea42cb5e5eb092997340b393cd14e41a36f
tampham512 <phamthanhtam512@gmail.com>
2023-09-14 21:08:33 +0700

    test : template

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -32,7 +32,6 @@ function App() {
   return (
     <ChakraBaseProvider theme={theme}>
       <ModalProvider>
-        test
         <GrammarForWriting />
       </ModalProvider>
     </ChakraBaseProvider>
