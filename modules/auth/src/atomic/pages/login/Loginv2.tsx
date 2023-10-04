cb3a7952f7e8240f8666d404856f5d9b04b7eb6b
Pham Thanh Tam <tam.pham-thanh@internal.banvien.com>
2023-10-04 10:10:45 +0700

    feat: rename

---
+++ modules/auth/src/atomic/pages/login/Loginv2.tsx
@@ -0,0 +1,10 @@
+import { LoginProvider } from "./LoginContext";
+import LoginApp from "./LoginApp";
+import { ComponentInject } from "@org/ui";
+import { AuthTemplate } from "../../templates";
+
+export const LoginPage = ComponentInject({
+  template: [AuthTemplate],
+  providers: [LoginProvider],
+  bootstrap: LoginApp,
+});
