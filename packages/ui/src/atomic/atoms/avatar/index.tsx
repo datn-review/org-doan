9e1bf7fa804aa59bb57cb76cb4c0d17060744d5a
Pham Thanh Tam <tam.pham-thanh@internal.banvien.com>
2023-10-04 09:52:18 +0700

    feat: rename

--- packages/ui/src/atomic/atoms/avatar/index.tsx
+++ packages/ui/src/atomic/atoms/avatar/index.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-// import { Button } from "src/atomic/atoms";
 import { Avatar as AvatarBase, AvatarProps } from "antd";
 export { type MenuProps } from "antd";
 export function Avatar(rest: AvatarProps) {
