57b27b8c6f3be84fcada2cd81ca7092bd3f49fc4
Pham Thanh Tam <tam.pham-thanh@internal.banvien.com>
2023-12-07 16:44:17 +0700

    feat: chat bot

--- apps/api/src/modules/chat/chat-bot/chat-bot.module.ts
+++ apps/api/src/modules/chat/chat-bot/chat-bot.module.ts
@@ -5,9 +5,10 @@ import { IsNotExist } from 'src/utils/validators/is-not-exists.validator';
 import { ChatBot } from './entities/chat-bot.entity';
 import { ChatBotController } from './chat-bot.controller';
 import { ChatBotService } from './chat-bot.service';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ChatBot])],
+  imports: [TypeOrmModule.forFeature([ChatBot]), UsersModule],
   controllers: [ChatBotController],
   providers: [IsExist, IsNotExist, ChatBotService],
   exports: [ChatBotService],
