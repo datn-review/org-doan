e99f868e5670dea34f3cf226f9eb5c5b91cfcfd7
tampham512 <phamthanhtam512@gmail.com>
2023-12-23 15:21:37 +0700

    deploy: bugs

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-if (import.meta.hot) import.meta.hot.accept(() => import.meta.hot.invalidate());
+// if (import.meta.hot) import.meta.hot.accept(() => import.meta.hot.invalidate());
 // if (process.env.NODE_ENV === 'production') {
 //   console.log = () => {
 //     //remove log
