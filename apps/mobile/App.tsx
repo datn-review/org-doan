aefd77cac01a0b4627e9e885b7e0a664e72fe10a
Pham Thanh Tam <104890135+tampham512@users.noreply.github.com>
2024-01-17 20:38:38 +0700

    Update App.tsx

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -79,7 +79,7 @@ function App(): JSX.Element {
           }}>
           <Section title="Step One">
             Edit <Text style={styles.highlight}>App.tsx</Text>
-            Tam Ne test
+            Tam Ne test build
             {COLOR.Primary}
           </Section>
           <Section title="See Your Changes">
